8afaa86279a600a4d11c412d06734f32927c4377
Lim Gim Hong <gimhong@sph.com.sg>
2018-02-13 10:54:40 +0800

    first commit

---
+++ Book Inventory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Book Inventory
+//
+//  Created by Lim Gim Hong on 13/2/18.
+//  Copyright © 2018 techTest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
